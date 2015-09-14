17dd1edfca324713ee2760213729d2aa33091cb0
lferreira457 <lferreira457@users.noreply.github.com>
2015-09-14 15:15:19 +0000

    added username to users table and signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :categories
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts do
     resources :comments
   end
